62791abb2813cb12fc9f7e439c1723e5dc0f68fe
fairuzaattallah <113004459+fairuzaattallah@users.noreply.github.com>
2023-03-13 13:32:31 +0700

    Add files via upload

---
+++ bubblesortdescending.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j,n,data[100],simpan,k;
+    cout<<"Pengurutan Data Descending"<<endl<<endl;
+    cout<<"Masukkan Banyak Data = ";
+    cin>>n;
+    cout<<""<<endl;
+
+    for (i=1; i<=n; i++)
+    {
+        cout<<"Data "<<i<<" = ";
+        cin>>data[i];
+    }
+    cout<<""<<endl;
+
+    cout<<"Awalnya  = ";
+    for (i=1; i<=n; i++)
+    {
+        cout<<data[i]<<" ";
+    }
+    cout<<""<<endl;
+
+    for (i=1; i<n; i++)
+    {
+        for (j=1; j<n; j++)
+        {
+            if (data[j]<data[j+1])
+            {
+                simpan=data[j];
+                data[j]=data[j+1];
+                data[j+1]=simpan;
+            }
+        }
+    }
+
+    cout<<"Hasilnya = ";
+    for (i=1; i<=n; i++)
+    {
+        cout<<data[i]<<" ";
+    }
+    cout<<""<<endl;
+    return 0;
+}
